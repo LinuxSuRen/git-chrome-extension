45e6607b8e8f25a7a524dc7771b55eb1d24e59db
Rick <linuxsuren@users.noreply.github.com>
2023-11-11 13:35:36 +0800

    fix: the github element changed
    update the gitpod icon image path

--- github-pr.js
+++ github-pr.js
@@ -4,6 +4,6 @@ const sourceBranch = meta.getElementsByTagName('a')[2]
 const gitpod = document.createElement('a')
 gitpod.href='https://gitpod.io/#' + sourceBranch.href
 gitpod.innerText='Open with GitPod'
-gitpod.innerHTML='<img src="https://gitpod.io/static/media/gitpod.2cdd910d.svg"/>'
+gitpod.innerHTML='<img height="28px" src="https://gitpod.io/favicon256.png"/>'
 gitpod.target='_blank'
 meta.append(gitpod)
